9de35204b88f69f4982e38c39daf1b886b1e0b68
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-03-14 14:37:26 -0500

    trying to figure this out

--- calculator.sh
+++ calculator.sh
@@ -1,4 +1,11 @@
-@echo off
-Calculator
-echo Greetings! What do you want to calculate?
-pause
+#!/bin/bash
+#Calculator
+echo Greetings! What do you want to Calculate?
+echo
+echo Instructions:
+echo	-Include a space between number and operation sign
+echo	-Use \\* instead of \* to multipy
+echo	
+read -p "Equation: " equation;
+echo
+expr $equation
